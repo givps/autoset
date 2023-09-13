75d2725a1372f6c4e3df6603d03a9d5e7d7fff86
mrxnext <142704070+mrxnext@users.noreply.github.com>
2023-09-14 01:46:37 +0700

    Update grpcupdate.sh

--- update/grpcupdate.sh
+++ update/grpcupdate.sh
@@ -6,10 +6,10 @@ cd
 
 cd /usr/bin
 
-wget -O addgrpc "https://raw.githubusercontent.com/givpn/autoset/master/xray/addgrpc.sh"
-wget -O cekgrpc "https://raw.githubusercontent.com/givpn/autoset/master/xray/cekgrpc.sh"
-wget -O delgrpc "https://raw.githubusercontent.com/givpn/autoset/master/xray/delgrpc.sh"
-wget -O renewgrpc "https://raw.githubusercontent.com/givpn/autoset/master/xray/renewgrpc.sh"
+wget -O addgrpc "https://raw.githubusercontent.com/givps/autoset/master/xray/addgrpc.sh"
+wget -O cekgrpc "https://raw.githubusercontent.com/givps/autoset/master/xray/cekgrpc.sh"
+wget -O delgrpc "https://raw.githubusercontent.com/givps/autoset/master/xray/delgrpc.sh"
+wget -O renewgrpc "https://raw.githubusercontent.com/givps/autoset/master/xray/renewgrpc.sh"
 
 
 chmod +x addgrpc
